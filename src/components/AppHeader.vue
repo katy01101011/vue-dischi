bb0107f2d6e993291cabef440488959bb2cf46bd
katy01101011 <katy.91560@gmail.com>
2022-05-19 17:04:30 +0200

    add: header (no stile)

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,26 @@
+<template>
+  <header>
+    <a href="">
+      <img class="mx-5" src="../assets/logo.svg" alt="" />
+    </a>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "AppHeader",
+};
+</script>
+
+<style lang="scss" scoped>
+header {
+  width: 100%;
+  height: 100px;
+  background-color: gray;
+  line-height: 100px;
+
+  img {
+    height: 80px;
+  }
+}
+</style>
